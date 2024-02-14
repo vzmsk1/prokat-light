5606fbb1acea4f30e964fd6b4018d7dfa0cf3db2
vyazemskaya <vyazemskayanna@gmail.com>
2024-02-14 23:08:13 +0300

    add: choose section

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -2,9 +2,9 @@
  * set hash to url
  * @param {string} hash
  */
-export const setHash = hash => {
-  hash = hash ? `#${hash}` : window.location.href.split('#')[0];
-  history.pushState('', '', hash);
+export const setHash = (hash) => {
+    hash = hash ? `#${hash}` : window.location.href.split('#')[0];
+    history.pushState('', '', hash);
 };
 
 /**
@@ -12,42 +12,42 @@ export const setHash = hash => {
  * @returns string
  */
 export const getHash = () => {
-  if (location.hash) {
-    return location.hash.replace('#', '');
-  }
+    if (location.hash) {
+        return location.hash.replace('#', '');
+    }
 };
 
 /**
  * initializes hamburger menu
  */
 export const menuInit = () => {
-  if (document.querySelector('.hamburger')) {
-    document.addEventListener('click', function (e) {
-      if (bodyLockStatus && e.target.closest('.hamburger')) {
-        menuOpen();
-      } else if (
-        bodyLockStatus &&
-        document.documentElement.classList.contains('_menu-opened') &&
-        (e.target.closest('.menu__close-btn') || !e.target.closest('.menu'))
-      ) {
-        menuClose();
-      }
-    });
-  }
+    if (document.querySelector('.hamburger')) {
+        document.addEventListener('click', function (e) {
+            if (bodyLockStatus && e.target.closest('.hamburger')) {
+                menuOpen();
+            } else if (
+                bodyLockStatus &&
+                document.documentElement.classList.contains('_menu-opened') &&
+                (e.target.closest('.menu__close-btn') || !e.target.closest('.menu'))
+            ) {
+                menuClose();
+            }
+        });
+    }
 };
 /**
  * opens hamburger menu
  */
 export const menuOpen = () => {
-  bodyLock();
-  document.documentElement.classList.add('_menu-opened');
+    bodyLock();
+    document.documentElement.classList.add('_menu-opened');
 };
 /**
  * closes hamburger menu
  */
 export const menuClose = () => {
-  bodyUnlock();
-  document.documentElement.classList.remove('_menu-opened');
+    bodyUnlock();
+    document.documentElement.classList.remove('_menu-opened');
 };
 
 // body lock
@@ -57,40 +57,40 @@ export let bodyLockStatus = true;
  * @param {number} delay
  */
 export const bodyLockToggle = (delay = 500) => {
-  if (document.documentElement.classList.contains('lock')) {
-    bodyUnlock(delay);
-  } else {
-    bodyLock(delay);
-  }
+    if (document.documentElement.classList.contains('lock')) {
+        bodyUnlock(delay);
+    } else {
+        bodyLock(delay);
+    }
 };
 /**
  * unlocks body
  * @param {number} delay
  */
 export const bodyUnlock = (delay = 500) => {
-  if (bodyLockStatus) {
-    setTimeout(() => {
-      document.documentElement.classList.remove('lock');
-    }, delay);
-    bodyLockStatus = false;
-    setTimeout(function () {
-      bodyLockStatus = true;
-    }, delay);
-  }
+    if (bodyLockStatus) {
+        setTimeout(() => {
+            document.documentElement.classList.remove('lock');
+        }, delay);
+        bodyLockStatus = false;
+        setTimeout(function () {
+            bodyLockStatus = true;
+        }, delay);
+    }
 };
 /**
  * locks body
  * @param {number} delay
  */
 export const bodyLock = (delay = 500) => {
-  if (bodyLockStatus) {
-    document.documentElement.classList.add('lock');
+    if (bodyLockStatus) {
+        document.documentElement.classList.add('lock');
 
-    bodyLockStatus = false;
-    setTimeout(function () {
-      bodyLockStatus = true;
-    }, delay);
-  }
+        bodyLockStatus = false;
+        setTimeout(function () {
+            bodyLockStatus = true;
+        }, delay);
+    }
 };
 
 /**
@@ -99,9 +99,9 @@ export const bodyLock = (delay = 500) => {
  * @returns
  */
 export function uniqueArray(array) {
-  return array.filter(function (item, index, self) {
-    return self.indexOf(item) === index;
-  });
+    return array.filter(function (item, index, self) {
+        return self.indexOf(item) === index;
+    });
 }
 
 /**
@@ -111,61 +111,52 @@ export function uniqueArray(array) {
  * process media requests from attributes
  */
 export const dataMediaQueries = (array, dataSetValue) => {
-  // get objects with media queries
-  const media = Array.from(array).filter(function (item, index, self) {
-    if (item.dataset[dataSetValue]) {
-      return item.dataset[dataSetValue].split(',')[0];
-    }
-  });
-  // objects with media queries initialization
-  if (media.length) {
-    const breakpointsArray = [];
-    media.forEach(item => {
-      const params = item.dataset[dataSetValue];
-      const breakpoint = {};
-      const paramsArray = params.split(',');
-      breakpoint.value = paramsArray[0];
-      breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : 'max';
-      breakpoint.item = item;
-      breakpointsArray.push(breakpoint);
+    // get objects with media queries
+    const media = Array.from(array).filter(function (item, index, self) {
+        if (item.dataset[dataSetValue]) {
+            return item.dataset[dataSetValue].split(',')[0];
+        }
     });
-    // get unique breakpoints
-    let mdQueries = breakpointsArray.map(function (item) {
-      return (
-        '(' +
-        item.type +
-        '-width: ' +
-        item.value +
-        'px),' +
-        item.value +
-        ',' +
-        item.type
-      );
-    });
-    mdQueries = uniqueArray(mdQueries);
-    const mdQueriesArray = [];
+    // objects with media queries initialization
+    if (media.length) {
+        const breakpointsArray = [];
+        media.forEach((item) => {
+            const params = item.dataset[dataSetValue];
+            const breakpoint = {};
+            const paramsArray = params.split(',');
+            breakpoint.value = paramsArray[0];
+            breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : 'max';
+            breakpoint.item = item;
+            breakpointsArray.push(breakpoint);
+        });
+        // get unique breakpoints
+        let mdQueries = breakpointsArray.map(function (item) {
+            return '(' + item.type + '-width: ' + item.value + 'px),' + item.value + ',' + item.type;
+        });
+        mdQueries = uniqueArray(mdQueries);
+        const mdQueriesArray = [];
 
-    if (mdQueries.length) {
-      // work with every breakpoint
-      mdQueries.forEach(breakpoint => {
-        const paramsArray = breakpoint.split(',');
-        const mediaBreakpoint = paramsArray[1];
-        const mediaType = paramsArray[2];
-        const matchMedia = window.matchMedia(paramsArray[0]);
-        // objects with conditions
-        const itemsArray = breakpointsArray.filter(function (item) {
-          if (item.value === mediaBreakpoint && item.type === mediaType) {
-            return true;
-          }
-        });
-        mdQueriesArray.push({
-          itemsArray,
-          matchMedia,
-        });
-      });
-      return mdQueriesArray;
+        if (mdQueries.length) {
+            // work with every breakpoint
+            mdQueries.forEach((breakpoint) => {
+                const paramsArray = breakpoint.split(',');
+                const mediaBreakpoint = paramsArray[1];
+                const mediaType = paramsArray[2];
+                const matchMedia = window.matchMedia(paramsArray[0]);
+                // objects with conditions
+                const itemsArray = breakpointsArray.filter(function (item) {
+                    if (item.value === mediaBreakpoint && item.type === mediaType) {
+                        return true;
+                    }
+                });
+                mdQueriesArray.push({
+                    itemsArray,
+                    matchMedia
+                });
+            });
+            return mdQueriesArray;
+        }
     }
-  }
 };
 
 /**
@@ -175,39 +166,39 @@ export const dataMediaQueries = (array, dataSetValue) => {
  * @param {boolean} showmore
  */
 export const _slideUp = (target, duration = 500, showmore = 0) => {
-  if (!target.classList.contains('_slide')) {
-    target.classList.add('_slide');
-    target.style.transitionProperty = 'height, margin, padding';
-    target.style.transitionDuration = duration + 'ms';
-    target.style.height = `${target.offsetHeight}px`;
-    target.offsetHeight;
-    target.style.overflow = 'hidden';
-    target.style.height = showmore ? `${showmore}rem` : `0`;
-    target.style.paddingTop = 0;
-    target.style.paddingBottom = 0;
-    target.style.marginTop = 0;
-    target.style.marginBottom = 0;
-    window.setTimeout(() => {
-      target.hidden = !showmore ? true : false;
-      !showmore ? target.style.removeProperty('height') : null;
-      target.style.removeProperty('padding-top');
-      target.style.removeProperty('padding-bottom');
-      target.style.removeProperty('margin-top');
-      target.style.removeProperty('margin-bottom');
-      !showmore ? target.style.removeProperty('overflow') : null;
-      target.style.removeProperty('transition-duration');
-      target.style.removeProperty('transition-property');
-      target.classList.remove('_slide');
-      // create event
-      document.dispatchEvent(
-        new CustomEvent('slideUpDone', {
-          detail: {
-            target: target,
-          },
-        })
-      );
-    }, duration);
-  }
+    if (!target.classList.contains('_slide')) {
+        target.classList.add('_slide');
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = `${target.offsetHeight}px`;
+        target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = showmore ? `${showmore}rem` : `0`;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        window.setTimeout(() => {
+            target.hidden = !showmore ? true : false;
+            !showmore ? target.style.removeProperty('height') : null;
+            target.style.removeProperty('padding-top');
+            target.style.removeProperty('padding-bottom');
+            target.style.removeProperty('margin-top');
+            target.style.removeProperty('margin-bottom');
+            !showmore ? target.style.removeProperty('overflow') : null;
+            target.style.removeProperty('transition-duration');
+            target.style.removeProperty('transition-property');
+            target.classList.remove('_slide');
+            // create event
+            document.dispatchEvent(
+                new CustomEvent('slideUpDone', {
+                    detail: {
+                        target: target
+                    }
+                })
+            );
+        }, duration);
+    }
 };
 
 /**
@@ -217,41 +208,41 @@ export const _slideUp = (target, duration = 500, showmore = 0) => {
  * @param {boolean} showmore
  */
 export const _slideDown = (target, duration = 500, showmore = 0) => {
-  if (!target.classList.contains('_slide')) {
-    target.classList.add('_slide');
-    target.hidden = target.hidden ? false : null;
-    showmore ? target.style.removeProperty('height') : null;
-    let height = target.offsetHeight;
-    target.style.overflow = 'hidden';
-    target.style.height = showmore ? `${showmore}rem` : `0`;
-    target.style.paddingTop = 0;
-    target.style.paddingBottom = 0;
-    target.style.marginTop = 0;
-    target.style.marginBottom = 0;
-    target.offsetHeight;
-    target.style.transitionProperty = 'height, margin, padding';
-    target.style.transitionDuration = duration + 'ms';
-    target.style.height = height + 'px';
-    target.style.removeProperty('padding-top');
-    target.style.removeProperty('padding-bottom');
-    target.style.removeProperty('margin-top');
-    target.style.removeProperty('margin-bottom');
-    window.setTimeout(() => {
-      target.style.removeProperty('height');
-      target.style.removeProperty('overflow');
-      target.style.removeProperty('transition-duration');
-      target.style.removeProperty('transition-property');
-      target.classList.remove('_slide');
-      // create event
-      document.dispatchEvent(
-        new CustomEvent('slideDownDone', {
-          detail: {
-            target: target,
-          },
-        })
-      );
-    }, duration);
-  }
+    if (!target.classList.contains('_slide')) {
+        target.classList.add('_slide');
+        target.hidden = target.hidden ? false : null;
+        showmore ? target.style.removeProperty('height') : null;
+        let height = target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = showmore ? `${showmore}rem` : `0`;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        target.offsetHeight;
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = height + 'px';
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        window.setTimeout(() => {
+            target.style.removeProperty('height');
+            target.style.removeProperty('overflow');
+            target.style.removeProperty('transition-duration');
+            target.style.removeProperty('transition-property');
+            target.classList.remove('_slide');
+            // create event
+            document.dispatchEvent(
+                new CustomEvent('slideDownDone', {
+                    detail: {
+                        target: target
+                    }
+                })
+            );
+        }, duration);
+    }
 };
 
 /**
@@ -261,11 +252,11 @@ export const _slideDown = (target, duration = 500, showmore = 0) => {
  * @returns function
  */
 export const _slideToggle = (target, duration = 500) => {
-  if (target.hidden) {
-    return _slideDown(target, duration);
-  } else {
-    return _slideUp(target, duration);
-  }
+    if (target.hidden) {
+        return _slideDown(target, duration);
+    } else {
+        return _slideUp(target, duration);
+    }
 };
 
 /**
@@ -274,13 +265,11 @@ export const _slideToggle = (target, duration = 500) => {
  * @returns string
  */
 export function remToPx(remValue) {
-  const htmlFontSize = parseFloat(
-    getComputedStyle(document.documentElement).fontSize
-  );
+    const htmlFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
 
-  const pxValue = remValue * htmlFontSize;
+    const pxValue = remValue * htmlFontSize;
 
-  return Math.round(pxValue) + 'px';
+    return Math.round(pxValue) + 'px';
 }
 
 /**
@@ -289,7 +278,16 @@ export function remToPx(remValue) {
  * @param {string} className
  */
 export const removeClasses = (array, className) => {
-  for (var i = 0; i < array.length; i++) {
-    array[i].classList.remove(className);
-  }
+    for (var i = 0; i < array.length; i++) {
+        array[i].classList.remove(className);
+    }
+};
+
+/**
+ * sets content to container target
+ * @param {string} content
+ * @param {HTMLElement} container
+ */
+export const setInnerContent = (content, container) => {
+    if (content && container) container.innerHTML = content;
 };
